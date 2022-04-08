d04c18fd67bf1efcac6671ef486c6823eef6baf4
JaviKo500 <javikogutierrez64@gmail.com>
2022-04-07 22:18:33 -0500

    Design portfolio

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { PortfolioComponent } from './portfolio.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 import { SkillsComponent } from './pages/skills/skills.component';
@@ -14,12 +13,8 @@ const routes: Routes = [
     component: PortfolioComponent,
     children: [
       {
-        path: '',
+        path: 'home',
         component: HomeComponent
-      },
-      {
-        path: 'aboutme',
-        component: AboutMeComponent
       },
       {
         path: 'projects',
@@ -35,7 +30,7 @@ const routes: Routes = [
       },
       {
         path: '**',
-        redirectTo: ''
+        redirectTo: 'home'
       }
     ]
   }
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -7,14 +7,13 @@ import { SharedModule } from '../shared/shared.module';
 
 import { HomeComponent } from './pages/home/home.component';
 import { PortfolioComponent } from './portfolio.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 import { SkillsComponent } from './pages/skills/skills.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, PortfolioComponent, AboutMeComponent, ProjectsComponent, ContactMeComponent, SkillsComponent],
+  declarations: [HomeComponent, PortfolioComponent, ProjectsComponent, ContactMeComponent, SkillsComponent],
   imports: [
     CommonModule,
     PortfolioRoutingModule,
